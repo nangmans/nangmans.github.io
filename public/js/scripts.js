14358000d462789e7bff1938c1df4d76bdf5c18b
nangmans <45918816+nangmans@users.noreply.github.com>
2020-04-29 18:37:48 +0900

    modify web

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,8 @@
-/*!
-    * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
+    (function ($) {
+    "use strict"; 
+
+
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
@@ -30,18 +26,17 @@
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
+
     $("body").scrollspy({
         target: "#mainNav",
         offset: 74,
     });
 
-    // Collapse Navbar
+
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
@@ -49,8 +44,19 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
-    // Collapse now if page is not at top
     navbarCollapse();
-    // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+})(jQuery); 
+
+document.getElementById("blog").addEventListener("click", function(){
+    window.location = 'https://nangman14.tistory.com/';
+    });
+
+document.getElementById("daeran").addEventListener("click", function(){
+    window.location = 'http://awsdjangoboard.eba-bspdpztp.ap-northeast-2.elasticbeanstalk.com/boardapp/';
+    });
+
+document.getElementById("imagenation").addEventListener("click", function(){
+    window.location = 'http://imagenation.ap-northeast-2.elasticbeanstalk.com/';
+    });
+    
